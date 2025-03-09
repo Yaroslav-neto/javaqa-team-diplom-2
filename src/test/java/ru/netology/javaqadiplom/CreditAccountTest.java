c53bf4beddefa6aa8398f717f38d4ee0b17d4497
Yar neto <quatro30@gmail.com>
2025-03-09 23:19:09 +0700

    change CreditAccountTest

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -3,18 +3,153 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CreditAccountTest {
 
-    @Test
+    @Test // начальный баланс 0
     public void shouldAddToPositiveBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                5_000,
-                15
-        );
+        CreditAccount account = new CreditAccount(0, 5_000, 15);
 
         account.add(3_000);
 
         Assertions.assertEquals(3_000, account.getBalance());
+    }
+
+    @Test
+        // начальный баланс меньше 0 выброс исключения
+    void shouldIllegalArgumentExceptionNegativeInitialBalance() {
+        assertThrows(IllegalArgumentException.class, () -> new CreditAccount(-100, 1000, 10));
+    }
+
+    @Test
+        // кредитный лимит 0 нет исключения
+    void shouldCreditLimitNull() {
+        assertDoesNotThrow(() -> new CreditAccount(1000, 0, 10));
+    }
+
+    @Test
+        // БАГ
+        //    кредитный лимит меньше 0 выброс исключения
+    void shouldCreditLimitLessNull() {
+        assertThrows(IllegalArgumentException.class, () -> new CreditAccount(1000, -1, 10));
+    }
+
+    @Test
+        //БАГ
+        //    ставка 0 нет исключения
+    void testNullRate() {
+        assertDoesNotThrow(() -> new CreditAccount(100, 1000, 0));
+    }
+
+    @Test
+        //    ставка меньше 0 выброс исключения
+    void testLessNullRate() {
+        assertThrows(IllegalArgumentException.class, () -> new CreditAccount(100, 1000, -1));
+    }
+
+    @Test
+        //БАГ
+        // успешная покупка
+    void testPaySuccessful() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertTrue(account.pay(50));
+        assertEquals(50, account.getBalance());
+    }
+
+    @Test
+        //БАГ
+        // неуспешная покупка
+    void testPayUnsuccessful() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.pay(1200));
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+        //БАГ
+        // положительный баланс - процентов нет
+    void testYearChangePositiveBalance() {
+        CreditAccount account = new CreditAccount(200, 1000, 15);
+        assertEquals(0, account.yearChange());
+    }
+
+    @Test
+        // отрицательный баланс - проценты есть
+    void testYearChangeNegativeBalance() {
+        CreditAccount account = new CreditAccount(-200, 1000, 15);
+        assertEquals(-30, account.yearChange());
+    }
+
+    @Test
+        //покупка на отрицательную сумму - 0 реакции
+    void testPayNegativeAmount() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.pay(-50));
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+        // пополнение на отрицательную сумму - 0 реакции
+    void testAddNegativeAmount() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.add(-50));
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+        //БАГ
+        // Покупка на сумму = лимиту + текущий баланс
+    void testPayUpToCreditLimit() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertTrue(account.pay(1100));
+        assertEquals(-1000, account.getBalance());
+    }
+
+    @Test
+        //БАГ
+        // покупка больше, чем кредитный лимит + текущий баланс
+    void testPayOverCreditLimit() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.pay(1101));
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+        // Пополнение счета на 0
+    void testAddZeroAmount() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.add(0));
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+        // Покупка на 0
+    void testPayZeroAmount() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.pay(0));
+        assertEquals(100, account.getBalance());
+    }
+
+    @Test
+        // Проверка начисления процентов при нулевом балансе
+    void testYearChangeZeroBalance() {
+        CreditAccount account = new CreditAccount(0, 1000, 15);
+        assertEquals(0, account.yearChange());
+    }
+
+    @Test
+        // Проверка процентов при балансе близком к нулю
+    void testYearChangeSmallBalance() {
+        CreditAccount account = new CreditAccount(-1, 1000, 15);
+        assertEquals(0, account.yearChange());
+    }
+
+    @Test
+        // пополнение на огромную сумму - баланс не изменится false
+    void testAddLargeAmount() {
+        CreditAccount account = new CreditAccount(100, 1000, 10);
+        assertFalse(account.add((int) Long.MAX_VALUE));
+        assertEquals(100, account.getBalance());
     }
 }
